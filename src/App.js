ada68e77d36557531b8093e91c83cf4ddb5a4de4
Sanketh Hegde <san2heg@gmail.com>
2019-03-25 15:59:09 -0700

    Setup timing and player movement animation

--- src/App.js
+++ src/App.js
@@ -57,6 +57,7 @@ class Map extends Component {
   // 2. MapSVG
   // 3. Nodes
   render() {
+    console.log(`${this.props.prevYear} => ${this.props.year} - ${this.props.percentComplete}`);
     // Populate list of nodes representing each team
     let teamElements = [];
     for (let key in TEAM_LOC_MAP) {
@@ -83,23 +84,38 @@ class Map extends Component {
     // Populate list of nodes representing players
     let playerElements = [];
     if (this.players != null) {
-      for (let key in this.players[this.props.year]) {
-        let player = this.players[this.props.year][key];
-        if (TEAM_LOC_MAP[player.team] == null) continue;
+      let prevYearPlayers = this.players[this.props.prevYear];
+      let nextYearPlayers = this.players[this.props.year];
+      for (let key in nextYearPlayers) {
+        let prevPlayer = prevYearPlayers[key]; // null if player is not in next year rankings
+        let nextPlayer = nextYearPlayers[key];
 
-        let stateElem = document.getElementById(TEAM_LOC_MAP[player.team]['svg-id']);
-        if (stateElem == null) {
+        if (TEAM_LOC_MAP[nextPlayer.team] == null) continue;
+
+        let nextStateElem = document.getElementById(TEAM_LOC_MAP[nextPlayer.team]['svg-id']);
+        let prevStateElem = prevPlayer == null ? nextStateElem : document.getElementById(TEAM_LOC_MAP[prevPlayer.team]['svg-id']);
+
+        if (prevStateElem == null || nextStateElem == null) {
           break;
         }
-        let rect = stateElem.getBoundingClientRect();
+        let prevRect = prevStateElem.getBoundingClientRect();
+        let nextRect = nextStateElem.getBoundingClientRect();
+
+        let topVal = prevRect.top + this.props.percentComplete * (nextRect.top - prevRect.top);
+        let leftVal = prevRect.left + this.props.percentComplete * (nextRect.left - prevRect.left);
+
+        let [nextXFrac, nextYFrac] = [TEAM_LOC_MAP[nextPlayer.team]['offset-x'], TEAM_LOC_MAP[nextPlayer.team]['offset-y']];
+        let nextOffsetX = nextXFrac == null ? 0 : nextXFrac * nextRect.width;
+        let nextOffsetY = nextYFrac == null ? 0 : nextYFrac * nextRect.height;
+        let [prevXFrac, prevYFrac] = prevPlayer == null ? [nextXFrac, nextYFrac] : [TEAM_LOC_MAP[prevPlayer.team]['offset-x'], TEAM_LOC_MAP[prevPlayer.team]['offset-y']];
+        let prevOffsetX = prevXFrac == null ? 0 : prevXFrac * prevRect.width;
+        let prevOffsetY = prevYFrac == null ? 0 : prevYFrac * prevRect.height;
+
+        let offsetXVal = prevOffsetX + this.props.percentComplete * (nextOffsetX - prevOffsetX);
+        let offsetYVal = prevOffsetY + this.props.percentComplete * (nextOffsetY - prevOffsetY);
+
         let imgPath = `http://localhost:5000/headshots/${key}.jpg`;
-
-        let [xFrac, yFrac] = [TEAM_LOC_MAP[player.team]['offset-x'], TEAM_LOC_MAP[player.team]['offset-y']];
-
-        let offsetX = xFrac == null ? 0 : xFrac * rect.width;
-        let offsetY = yFrac == null ? 0 : yFrac * rect.height;
-
-        playerElements.push(<Node type='player' key={key} top={rect.top} left={rect.left} offsetX={offsetX} offsetY={offsetY} imgpath={imgPath} />);
+        playerElements.push(<Node type='player' key={key} top={topVal} left={leftVal} offsetX={offsetXVal} offsetY={offsetYVal} imgpath={imgPath} />);
       }
     }
 
@@ -131,8 +147,11 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.currYear = new Date().getFullYear();
-    this.state = {'year': this.currYear};
+    this.state = {prevYear: this.currYear, year: this.currYear, percentComplete: 1.0};
+    this.timerIDs = [];
+
     this.adjustYear = this.adjustYear.bind(this);
+    this.animate = this.animate.bind(this);
   }
 
   // Adjust year with offset value
@@ -140,9 +159,26 @@ class App extends Component {
     let tempYear = this.state.year + offset;
     if (tempYear >= 1974 && tempYear <= this.currYear) {
       this.setState(state => ({
-        'year': tempYear
+        year: tempYear,
+        prevYear: state.year,
+        percentComplete: 0
       }));
+      this.timerIDs.push(setInterval(this.animate, 1));
     }
+  }
+
+  // Maintain animation state
+  animate() {
+    if (this.state.percentComplete >= 1) {
+      this.setState({
+        percentComplete: 1
+      });
+      clearInterval(this.timerIDs.pop());
+      return;
+    }
+    this.setState(state => ({
+      percentComplete: state.percentComplete + 0.005
+    }));
   }
 
   render() {
@@ -151,7 +187,7 @@ class App extends Component {
         <div className="title-container">
           <h1>NBA Player Map</h1>
         </div>
-        <Map year={this.state.year}/>
+        <Map year={this.state.year} year={this.state.year} prevYear={this.state.prevYear} percentComplete={this.state.percentComplete} />
         <div className="select-container">
           <YearSelector year={this.state.year} onYearChanged={this.adjustYear}/>
         </div>
